e7c7b41a97a706b4957824d1b1d5dacfc0fcf5e2
Vasko Jovanoski <vacikabre@gmail.com>
2023-05-26 20:24:59 +0100

    first commit

---
+++ src/main/java/com/vacika/projectstartertemplate/ProjectStarterTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.vacika.projectstartertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStarterTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStarterTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vacika/projectstartertemplate/ProjectStarterTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacika.projectstartertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStarterTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
